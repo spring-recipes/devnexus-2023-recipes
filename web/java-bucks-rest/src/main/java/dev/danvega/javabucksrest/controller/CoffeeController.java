5c6d24f2a3a18b8ea92898fef6aba6fdb490a31a
Dan Vega <danvega@gmail.com>
2023-03-27 15:35:21 -0400

    removing unused imports

--- web/java-bucks-rest/src/main/java/dev/danvega/javabucksrest/controller/CoffeeController.java
+++ web/java-bucks-rest/src/main/java/dev/danvega/javabucksrest/controller/CoffeeController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
