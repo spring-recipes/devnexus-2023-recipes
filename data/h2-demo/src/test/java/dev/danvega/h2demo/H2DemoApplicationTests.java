819a6a3c5245a19dbc58bf6da21f7f7f6961acb6
Dan Vega <danvega@gmail.com>
2023-03-28 09:26:51 -0400

    upgrading h2 demo to Spring Boot 3

---
+++ data/h2-demo/src/test/java/dev/danvega/h2demo/H2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danvega.h2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
