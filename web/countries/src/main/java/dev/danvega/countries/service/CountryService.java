7ab7435be23415f3bfc4438c45d9650aec8d5245
Dan Vega <danvega@gmail.com>
2023-03-29 11:13:26 -0400

    cleaning up the reactive code

--- web/countries/src/main/java/dev/danvega/countries/service/CountryService.java
+++ web/countries/src/main/java/dev/danvega/countries/service/CountryService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 public class CountryService {
 
-    private HttpGraphQlClient graphQlClient;
+    private final HttpGraphQlClient graphQlClient;
 
     public CountryService() {
         WebClient client = WebClient.builder()
